a7e13367d9682c36be9d69a2f6ef494c13df18e2
galya-cmd <galya1985@bk.ru>
2021-08-18 23:24:51 +0300

    еще одна карточка

--- heroes/src/components/heroes.jsx
+++ heroes/src/components/heroes.jsx
@@ -1,10 +1,13 @@
-function Heroes (){
+import './heroes.css'
+
+
+function Heroes(props) {
     return(
         <>
         <div className="heroes-cart">
-            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fcom-x.life%2F837-captain-marvel-v7-read.html&psig=AOvVaw3Zerd-oHCsBsY1YrQ5XnrR&ust=1629402106559000&source=images&cd=vfe&ved=0CAgQjRxqFwoTCPDVq4Gqu_ICFQAAAAAdAAAAABAD" alt=""/>
-            <div className="heroes-name">Капитан Марвел</div>
-            <div className="heroes-text">женщина непростой судьбы, а история ее появления сложновата для кино (она получает силы благодаря другому супергерою). За время своего существования Капитан сменила множество костюмов и позывных, сольный комикс у нее выходил далеко не всегда, да и вообще назвать ее очень уж популярной сложно. Увы, такова участь женских персонажей в комиксах. Однако даже при таком уровне известности она, пожалуй, главная супергероиня одноименного издательства</div>
+            <img src={props.url} alt=""/>
+            <div className="heroes-name">{props.name}</div>
+            <div className="heroes-text">{props.text}</div>
         </div>
 
         </>
