4740b3f73112c57cefbfea08aff977995d7ceb15
galya-cmd <galya1985@bk.ru>
2021-12-11 23:27:44 +0300

    создание второго компонента

--- heroes/src/components/flowers.jsx
+++ heroes/src/components/flowers.jsx
@@ -1,12 +1,15 @@
 import './heroes.css';
 
 
-function Flowers (props) {
-    return(
+
+function Flowers(props) {
+    return (
         <>
-        <div className="heroes-cart">
+            <div className="heroes-cart">
             <img src={props.url} alt=""/>
+            
             <div className="heroes-name">{props.name}</div>
+            <div>{props.size}</div>
             <div className="heroes-text">{props.text}</div>
         </div>
 
